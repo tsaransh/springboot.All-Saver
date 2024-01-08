79fa7a6ad075967e5ebdf5d986b89bdb92cba9d7
Saransh Tyagi <tyagisaransh90@gmail.com>
2024-01-08 18:46:53 +0530

    setup project

---
+++ src/main/java/com/web/allsaver/AllSaverApplication.java
@@ -0,0 +1,13 @@
+package com.web.allsaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllSaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/allsaver/AllSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.allsaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllSaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
